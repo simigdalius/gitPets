fac953d343d75e13177fda5621face6870706f93
simigdalius <150370765+simigdalius@users.noreply.github.com>
2026-03-18 22:53:43 +0200

    Update gitPets.py

--- gitPets.py
+++ gitPets.py
@@ -146,6 +146,5 @@ def generate_pet():
 
     return Response(svg_image, mimetype='image/svg+xml')
     
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
